84fc3d9eb38ad265c4e9f981931d28cca79e0146
PETR SOLDATOV <aulerok@gmail.com>
2022-03-07 00:26:57 +0300

    add info message

--- app.py
+++ app.py
@@ -1,15 +1,36 @@
 import telebot
+import time
 from extensions import ConvertionException, CriptoConverter
 from config import TOKEN, keys
 
 bot = telebot.TeleBot(TOKEN)
 
 
+@bot.message_handler(commands=['info'])
+def handle_info(message):
+    bot.send_message(message.chat.id, f"Это телеграм бот для конфертации валют.\n"
+                                      f"Пришлите мне сообщение\n"
+                                      f"'Название валюты которую надо перевести' 'валюту в которую переводим' "
+                                      f"'количество'\n"
+                                      f"Например  'доллар рубль 100'\n"
+                                      f"доступные валюты можно увидеть по команде /value\n"
+                                      f"чтобы еще раз увидеть это сообщение введите /info ")
+
+
 @bot.message_handler(commands=['start', 'help'])
 def handle_start_help(message):
     bot.send_message(message.chat.id, f"Привет {message.chat.username} дружочек чем могу помогу\n"
-                                      f"Всегда можно увидеть инструкцию вызвав команду /help\n"
+                                      "Всегда можно увидеть инструкцию вызвав команду /info\n"
                                       f"введите команду /value для просмотра доступных валют")
+
+    time.sleep(2)
+    bot.send_message(message.chat.id, f"Это телеграм бот для конфертации валют.\n"
+                                      f"Пришлите мне сообщение\n"
+                                      f"'Название валюты которую надо перевести' 'валюту в которую переводим' "
+                                      f"'количество'\n"
+                                      f"Например  'доллар рубль 100'\n"
+                                      f"доступные валюты можно увидеть по команде /value\n"
+                                      f"чтобы еще раз увидеть это сообщение введите /info ")
 
 
 @bot.message_handler(commands=['value'])
@@ -29,7 +50,7 @@ def convert(message: telebot.types.Message):
             raise ConvertionException("Слишком много параметров")
 
         quote, base, amount, = values
-        total_base = CriptoConverter.get_price(quote, base, amount)
+        total_base = CriptoConverter.get_price(quote.lower(), base.lower(), amount)
     except ConvertionException as e:
         bot.reply_to(message, f"Ошибка пользователя \n{e}")
     except Exception as e:
